f2ebdfe2754fb9c4e637a02e99e39df72f88b8f3
estark3 <estark3@charlotte.edu>
2025-02-14 15:37:56 -0500

    Adding N-body C++ code.

---
+++ nBodySim.cpp
@@ -0,0 +1,19 @@
+// Evan Stark - February 14th, 2025 - ITCS 4145 001
+// This program simulates a basic n body problem.
+// I.e. simulating the movement of particles in 3 dimensions.
+// Will track the particles force, position, mass, and velocity and use those variables to
+// continously update most of those said variables.
+
+// Importing all necessary libraries.
+#include <iostream>
+#include <cstdlib>
+#include <random>
+#include <chrono>
+#include <vector>
+
+// Vector that stores all a particle's variables.
+double* particle() {
+    NULL;
+}
+
+// Function for initializing a particles attributes.
